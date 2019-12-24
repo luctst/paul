a34862bb459769b395e8d79d751287068a526f5d
Lucas Tostée <lucas.tostee@gmail.com>
2019-12-24 19:46:28 +0100

    Start writing test

--- test/test.js
+++ test/test.js
@@ -1,2 +1,40 @@
 import test from "ava";
-test.todo("Implement test");
+import puppeteer from "puppeteer";
+import Paul from "../lib/index";
+let browser;
+let page;
+let ElementTest;
+
+test.before("Launch the chronium instance", async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    ElementTest = await page.$("head");
+
+    // await page.addScriptTag({path: "./lib/src/paul.js"});
+    
+    page.on("console", async msg => {
+        if (msg.text() === "JSHandle@object") {
+            console.log(await msg.args()[0].jsonValue());
+        } else {
+            console.log(msg);
+        }
+    });
+
+    page.on("error", msg => {
+        console.log(msg);
+    });
+});
+
+test("Check if first argument is an HTML Element", async t => {
+    await t.notThrowsAsync(async () => await page.evaluate(DOMElementTest => Paul(DOMElementTest), ElementTest));
+    await ElementTest.dispose();
+});
+
+test("Check if second argument is an array", async t => {
+    await t.notThrowsAsync(async () => await page.evaluate(DOMElementTest => Paul(DOMElementTest, []), ElementTest));
+    await ElementTest.dispose();
+});
+
+test.after("Close the chronium instance", async () => {
+    await browser.close();
+});
